11f8a98e028b2226547fc3668cd4627022aef7a6
oniboy6775 <onisabiabdullahi@gmail.com>
2024-06-06 14:05:18 +0100

    whatsapp link corrected

--- client/src/components/WhatsAppIcon.js
+++ client/src/components/WhatsAppIcon.js
@@ -5,7 +5,7 @@ import { FaWhatsapp } from "react-icons/fa";
 const WhatsAppIcon = () => {
   const whatsAppLink = () => {
     window.location.href =
-      "https://wa.me/2347031362263?text=Hello,%20myDataPort.com.ng";
+      "https://wa.me/2348096064724?text=Hello,%20myDataPort.com.ng";
   };
   return (
     <Container onClick={whatsAppLink}>
